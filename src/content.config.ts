8a3acbec3e4fe4d14dbaf8204af321d7e28a10cb
SH20RAJ <shaswatraj3@gmail.com>
2026-03-27 09:17:10 +0530

    blogs showing

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const writers = defineCollection({
   schema: z.object({
@@ -30,6 +31,7 @@ const collectionsData = defineCollection({
 });
 
 const blog = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
